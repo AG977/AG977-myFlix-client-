ab9f2b3bc08c1e1a66d5dca2ff207edcac99b9a3
AG977 <93376305+AG977@users.noreply.github.com>
2022-03-01 20:56:39 +0100

    added loginview, registrationview

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,28 @@
-  import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-  export class MovieCard extends React.Component {
-    render() {
-      const { movie, onMovieClick } = this.props;
-  
-      return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
-    }
+export class MovieCard extends React.Component {
+  render() {
+    const { movie, onMovieClick } = this.props;
+
+    return (
+      <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+    );
   }
- 
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
