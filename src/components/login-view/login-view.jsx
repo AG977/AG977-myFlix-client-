fd7dce758458feb739fbaff028bddcbc10c24845
AG977 <93376305+AG977@users.noreply.github.com>
2022-03-30 16:16:11 +0200

    changed class to className

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -27,9 +27,9 @@ export function LoginView(props) {
   return (
     <Container>
       <Row>
-        <Col class="login-padding">
+        <Col className="login-padding">
           <CardGroup>
-            <Card class="login">
+            <Card className="login">
               <Card.Body>
                 <Card.Title id="title">Login page!</Card.Title>
                 <Form>
