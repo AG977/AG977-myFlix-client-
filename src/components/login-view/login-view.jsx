c01cb2c3de552fe8d2787e7b2d0539e8ef166e9b
AG977 <93376305+AG977@users.noreply.github.com>
2022-04-01 17:45:44 +0200

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -18,7 +18,7 @@ export function LoginView(props) {
   .then(response => {
     const data = response.data;
     props.onLoggedIn(data);
-  })
+  }) 
   .catch(e => {
     console.log('no such user')
   });
