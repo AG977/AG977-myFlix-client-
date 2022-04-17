4373c3fa6575a485169079237632ff6606efb3b8
AG977 <93376305+AG977@users.noreply.github.com>
2022-04-17 19:53:34 +0200

    deleted unnecessary import

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,7 +4,6 @@ import './movie-view.scss';
 import Images from '../../../Images/*.png';
 import { Image, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import { DierctorView } from '../director-view/director-view';
 
 export class MovieView extends React.Component {
 
