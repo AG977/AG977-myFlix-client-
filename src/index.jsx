1a9c59d267ac25c0209a3ee0112d6c356d3710d4
AG977 <93376305+AG977@users.noreply.github.com>
2022-05-24 12:59:41 +0200

    added back createStore and remove curly brackets from Main

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,16 @@ import MainView from './components/main-view/main-view';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
 import Container from 'react-bootstrap/Container';
-import { configureStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { MainView } from './components/main-view/main-view';
+import MainView from './components/main-view/main-view';
 
 
 // Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
 //const store = createStore(moviesApp);
-const store = configureStore(moviesApp, devToolsEnhancer());
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
